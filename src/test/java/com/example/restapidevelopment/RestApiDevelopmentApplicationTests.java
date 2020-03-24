2e0a11c5b80c31e856213fa56ed3e12dd2595188
hemantjava <hemantjava90@gmail.com>
2020-03-24 14:43:30 +0530

    initial commit

---
+++ src/test/java/com/example/restapidevelopment/RestApiDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapidevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
