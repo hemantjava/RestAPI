dbf8db23a63f33b656a9182e054e9ef77afe4174
hemantjava <hemantjava90@gmail.com>
2020-03-25 18:35:58 +0530

    initial commit

--- src/main/java/com/example/restapidevelopment/SwaggerConfig.java
+++ src/main/java/com/example/restapidevelopment/SwaggerConfig.java
@@ -6,23 +6,27 @@ import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@SuppressWarnings("ALL")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    public static final Contact DEFAULT_CONTACT = new Contact(
-            "Ranga Karanam", "http://www.bookworld.com", "bookworld@gmail.com");
-
-    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-            "Awesome book world API Title", "Awesome book worldAPI Description", "1.0",
-            "urn:tos", DEFAULT_CONTACT.toString(),
-            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Demo of  REST API")
+                .contact(new Contact("Hemant Kumar sahu", "www.hemantworld.net", "hemantjava90@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
             new HashSet<String>(Arrays.asList("application/json",
@@ -31,8 +35,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT_API_INFO)
-                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
-                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+                .apiInfo(apiEndPointsInfo())
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
---
+++ src/main/java/com/example/restapidevelopment/dto/SomeBean.java
@@ -0,0 +1,16 @@
+package com.example.restapidevelopment.dto;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@JsonFilter("SomeBeanFilter")
+public class SomeBean {
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+}
