dbdc34d74ad0d6415f1b8bb919d247e13ca6f3ce
hemantjava <hemantjava90@gmail.com>
2020-03-27 17:20:23 +0530

    initial commit

--- src/main/java/com/example/restapidevelopment/entity/Person.java
+++ src/main/java/com/example/restapidevelopment/entity/Person.java
@@ -1,5 +1,6 @@
 package com.example.restapidevelopment.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Builder
 @Entity
 public class Person {
    @Id
