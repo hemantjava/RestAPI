9de833bb0261d6529a9fca800b620ae569a703cd
hemantjava <hemantjava90@gmail.com>
2020-07-23 12:33:47 +0530

    Null OR Empty checked for Collection.

---
+++ src/test/java/com/example/restapidevelopment/list/ListNullCheck.java
@@ -0,0 +1,32 @@
+package com.example.restapidevelopment.list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListNullCheck {
+
+    private boolean isNullOrEmpty(Collection collection) {
+        return collection == null || collection.isEmpty() ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    private boolean isNotNullOrEmpty(Collection collection) {
+        return collection == null || collection.isEmpty() ? Boolean.FALSE : Boolean.TRUE;
+    }
+
+    @Test
+    void testE() {
+        List<String> strings = getList();
+        System.out.println(isNullOrEmpty(strings));
+        System.out.println(isNotNullOrEmpty(strings));
+
+    }
+
+    private List<String> getList() {
+        List<String> strings = new LinkedList<>();
+        strings.add("Hemant");
+        return strings;
+    }
+}
